d458a0f9b6e850a67945126cd9c344bc975150b8
Sergio <serham_95@hotmail.com>
2024-11-05 18:05:50 -0500

    subiendo el challenge

---
+++ src/main/java/com/alura/challenge_libros/ChallengeLibrosApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challenge_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challenge_libros/ChallengeLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
