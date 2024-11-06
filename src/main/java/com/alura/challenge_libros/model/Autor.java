e685523c73e07f827f4fb283886701c1d06e6f3a
Sergio <serham_95@hotmail.com>
2024-11-05 22:28:42 -0500

    adicionando datos a autor

--- src/main/java/com/alura/challenge_libros/model/Autor.java
+++ src/main/java/com/alura/challenge_libros/model/Autor.java
@@ -4,10 +4,12 @@ public class Autor {
     private Long id;
     private String nombre;
     private Integer fechaNacimiento;
+    private Integer fechaDesceso;
 
-    public Autor(String nombre, Integer fechaNacimiento) {
+    public Autor(String nombre, Integer fechaNacimiento, Integer fechaDesceso) {
         this.nombre = nombre;
         this.fechaNacimiento = fechaNacimiento;
+        this.fechaDesceso = fechaDesceso;
     }
 
     public String getNombre() {
@@ -18,11 +20,16 @@ public class Autor {
         return fechaNacimiento;
     }
 
+    public Integer getFechaDesceso(){
+        return fechaDesceso;
+    }
+
     @Override
     public String toString() {
         return "Autor{" +
                 "nombre='" + nombre + '\'' +
                 ", fechaNacimiento=" + fechaNacimiento +
+                ", fechaDesceso=" + fechaDesceso +
                 '}';
     }
 }
